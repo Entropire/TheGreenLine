b06c3051038c2f94ae5eb9f8f217d4c17a6dfc80
Dev_ <patrykos1010@gmail.com>
2025-01-10 11:21:19 +0100

    Code Edits & Finishing touches

--- Assets/Dev's Code And Assets/UiNavigation.cs
+++ Assets/Dev's Code And Assets/UiNavigation.cs
@@ -7,41 +7,19 @@ using UnityEngine.SceneManagement;
 public class UiNavigation : MonoBehaviour
 {
     // Code made by Patryk.
-    // This code will control buttons that modify UIs and scenes. This means that this will switch between scenes and or open panels.
+    // This code will control buttons that modify UIs. This means that this will switch between UIs and Panels.
     // Mostly public values for interchangeability.
 
-    private void Start()
+    public void ToggleUI()
     {
+        GameObject uiParent = gameObject; // Defines the UIparent being the model the script is under.
 
-    }
-    public void ButtonFunct(bool Buttonversion)
-    {
-        GameObject Panel = gameObject;
-        if (Buttonversion == true) //Checks if the button is either used for scenechanges or gameobject management.
+        if (uiParent == null) // If uiParent is undefined or null
         {
-            if (Panel.activeInHierarchy == false)
-            {
-                Panel.SetActive(true);
-            }
-            else if (Panel.activeInHierarchy == true)
-            {
-                Panel.SetActive(false);
-            }
-            else
-            {
-                Debug.Log("Unexpected Error in Panelctrl True. Else called, undefined values?");
-                Panel.SetActive(false); //If error encountered, set panel to false to restart function.
-            }
+            Debug.Log("Error - Gameobject uiParent Undefined");
+            return; // Return if the uiParent is null to restart the function.
         }
-        else if (Buttonversion == false) //Loads the scene. Displays in Print how many scenes loaded for debugging.
-        {
-            SceneManager.LoadScene("TestSceneX");
-            print("Scenes Loaded = " + SceneManager.loadedSceneCount);
-        }
-        else
-        {
-            Debug.Log("Error, undefined values and or undefined bool in ButtonFunct");
-            Buttonversion = true; //If error encountered, set function to true, announce error.
-        }
+
+        uiParent.SetActive(!uiParent.activeInHierarchy); //Toggle the uiParent between its active and inactive state.
     }
 }
