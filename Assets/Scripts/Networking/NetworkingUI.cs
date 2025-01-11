eff4f2a36851b2b7a7e8346f263add1e685df964
Entropire <158569522+Entropire@users.noreply.github.com>
2025-01-11 19:36:39 +0100

    debug changes

--- Assets/Scripts/Networking/NetworkingUI.cs
+++ Assets/Scripts/Networking/NetworkingUI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Threading;
+using TMPro;
 using UnityEditor.Rendering.Universal;
 using UnityEngine;
 
@@ -7,21 +8,58 @@ namespace Server
 {
   internal class NetworkingUI : MonoBehaviour
   {
+    [SerializeField] private TMP_Text text;
+
     CancellationTokenSource cts = new CancellationTokenSource();
+    private SynchronizationContext mainThreadContext;
+
+    private void Start()
+    {
+      mainThreadContext = SynchronizationContext.Current;
+    }
 
     public void Client()
     {
       Client client = new Client();
-      client.onMessage += (msg) => Debug.Log(msg);
-      client.onError += (msg) => Debug.LogError(msg);
-      client.Start(IPAddress.Parse("192.168.178.195"), 8000, cts.Token);
+      client.onMessage += (msg) =>
+      {
+        mainThreadContext.Post(_ =>
+        {
+          Debug.Log(msg);
+          text.text = msg;
+        }, null);
+      };
+        client.onError += (msg) =>
+      {
+        mainThreadContext.Post(_ =>
+        {
+          Debug.Log(msg);
+          text.text = msg;
+        }, null);
+      };
+        client.Start(IPAddress.Parse("192.168.178.195"), 8000, cts.Token);
     }
 
     public void Host()
     {
       Host host = new Host();
-      host.onMessage += (msg) => Debug.Log(msg);
-      host.onError += (msg) => Debug.LogError(msg);
+      host.onMessage += (msg) =>
+      {
+        mainThreadContext.Post(_ =>
+        {
+          Debug.Log(msg);
+          text.text = msg;
+        }, null);
+      };
+      host.onError += (msg) =>
+      {
+        mainThreadContext.Post(_ =>
+        {
+          Debug.Log(msg);
+          text.text = msg;
+        }, null);
+      };
+      
       host.Start(IPAddress.Parse("192.168.178.195"), 8000, cts.Token);
     }
   }
